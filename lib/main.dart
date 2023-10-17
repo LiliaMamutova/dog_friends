556098f19c2589af8ec98ce04af9a80e4db0dbc8
Lilia Mamutova <lilymalinovskaya@gmail.com>
2023-10-18 00:06:55 +0300

    removed unnecessary directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:dog_friends/features/user/screens/user_profile_screen.dart';
 import 'package:dog_friends/theme_data/dark_theme/dark_theme.dart';
 import 'package:dog_friends/theme_data/light_theme/light_theme.dart';
 import 'package:dog_friends/value_notifier/vn.dart';
 import 'package:flutter/material.dart';
-
-import 'features/user/screens/user_profile_screen.dart';
 
 void main() {
   runApp(MyApp());
